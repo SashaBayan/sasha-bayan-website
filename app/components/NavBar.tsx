5f39311f649952682893c2842260020663357dd7
SashaBayan <sashabayan@gmail.com>
2023-09-16 10:51:14 -0700

    add fade-in border-b on nav links

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,21 +4,28 @@ export function NavBar() {
   return (
     <div className="flex w-full justify-end px-10 py-5 text-sm text-black">
       <div className="hidden gap-10 md:flex">
-        <div className="hover:animate-pulse">
-          <a href="#home" className="cursor-pointer ">
-            Home
-          </a>
-        </div>
+        <a
+          href="#home"
+          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
+        >
+          Home
+        </a>
         <a
           href="#music"
-          className="cursor-pointer border-black hover:animate-pulse hover:border-b"
+          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
         >
           Music
         </a>
-        <a href="#bio" className="cursor-pointer hover:animate-pulse">
+        <a
+          href="#bio"
+          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
+        >
           Bio
         </a>
-        <a href="#contact" className="cursor-pointer hover:animate-pulse">
+        <a
+          href="#contact"
+          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
+        >
           Contact
         </a>
       </div>
