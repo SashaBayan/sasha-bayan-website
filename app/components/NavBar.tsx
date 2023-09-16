8a5372cd6a8f0ac95f4887a896101fbc1fa91f12
SashaBayan <sashabayan@gmail.com>
2023-09-16 11:19:05 -0700

    WIP first section, make nav bar sit on top content
    Using -margin to handle this. Could be a fragile approach. Using relative container div did not work as expected

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,34 +1,30 @@
-import Image from "next/image";
-
 export function NavBar() {
   return (
-    <div className="flex w-full justify-end px-10 py-5 text-sm text-black">
-      <div className="hidden gap-10 md:flex">
-        <a
-          href="#home"
-          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
-        >
-          Home
-        </a>
-        <a
-          href="#music"
-          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
-        >
-          Music
-        </a>
-        <a
-          href="#bio"
-          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
-        >
-          Bio
-        </a>
-        <a
-          href="#contact"
-          className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
-        >
-          Contact
-        </a>
-      </div>
+    <div className="sticky top-0 z-10 -mb-20 flex w-full justify-end gap-10 px-10 py-5 text-sm text-black">
+      <a
+        href="#home"
+        className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
+      >
+        Home
+      </a>
+      <a
+        href="#music"
+        className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
+      >
+        Music
+      </a>
+      <a
+        href="#bio"
+        className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
+      >
+        Bio
+      </a>
+      <a
+        href="#contact"
+        className="transition-border-color cursor-pointer border-b border-transparent p-1 duration-300 hover:animate-pulse hover:border-black"
+      >
+        Contact
+      </a>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,26 @@ import { NavBar } from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen bg-white">
+    <main className="">
       <NavBar />
+      <section className="flex w-screen">
+        {/* left side */}
+        <div>
+          <Image
+            className="grayscale"
+            src="/hero-image.jpg"
+            width={720}
+            height={990}
+            alt="sasha bayan photo"
+          />
+        </div>
+        {/* right side */}
+        <div className="flex items-center justify-center bg-[#E8E1DD]">
+          <h1 className="text-5xl font-black">
+            Musician, collaborator, artist
+          </h1>
+        </div>
+      </section>
     </main>
   );
 }
