a4193b4dee1aa6c9dbbfa8826e22083d2fa8a43e
SashaBayan <sashabayan@gmail.com>
2023-09-16 11:39:54 -0700

    add content to right section of top-most section

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,41 @@ export default function Home() {
           />
         </div>
         {/* right side */}
-        <div className="flex flex-grow items-center justify-center bg-[#E8E1DD]">
-          <h1 className="text-5xl font-black">
-            Musician, collaborator, artist
-          </h1>
+        <div className="justify-left flex flex-grow items-center bg-[#E8E1DD] px-16 pt-20">
+          <div className="flex flex-col gap-16">
+            <div className="max-w-sm">
+              <div className="flex flex-col gap-8">
+                <h1 className="text-5xl font-black">
+                  Musician, collaborator, artist
+                </h1>
+                <p>
+                  Sasha Bayan is a classically trained multi-instrumentalist,
+                  composer, and singer-songwriter based in the Bay Area. He
+                  regularly performs solo and in ensembles. He performs
+                  classically-inspired sitar; classical, jazz, and Brazilian
+                  guitar; and his own original compositions and arrangements.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex flex-wrap gap-2">
+                <p>New album dropping November 2023!</p>
+                <button> Get notified.</button>
+              </div>
+              <Image
+                className="-ml-2"
+                width={575}
+                height={575}
+                src="/album-cover-placeholder.png"
+                alt="album cover"
+              />
+              <div className="flex w-full justify-center">
+                <button className="rounded-3xl bg-white px-5 py-3 text-xl">
+                  Preorder
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </main>
