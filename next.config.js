f68c746351360db02b3402db26f34a82386d7329
SashaBayan <sashabayan@gmail.com>
2023-10-04 11:39:18 -0700

    update next config to access cloudfront images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { optimizeFonts: true };
+const nextConfig = {
+  optimizeFonts: true,
+  images: {
+    domains: [
+      "d1h180s7hmcb7i.cloudfront.net",
+      "sasha-bayan-website.s3.us-west-1.amazonaws.com",
+    ],
+  },
+};
 
 module.exports = nextConfig;
