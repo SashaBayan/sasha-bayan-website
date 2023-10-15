8276b787d6dba57eb584e3f955b97cf15901323b
SashaBayan <sashabayan@gmail.com>
2023-10-14 17:24:52 -0700

    remove “hello” template

--- app/hello/page.tsx
+++
@@ -1,9 +0,0 @@
-import { sayHello } from "./utils";
-
-export default function Hello() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-pink-500 text-3xl">
-      {sayHello()}
-    </div>
-  );
-}
