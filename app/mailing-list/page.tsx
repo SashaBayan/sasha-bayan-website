79b28550e8a82658b8d09cd2d27a794d26640b28
SashaBayan <sashabayan@gmail.com>
2023-12-03 20:00:21 -0800

    use correct mailing list
    was previously high tide, needed to be SB

--- app/mailing-list/page.tsx
+++ app/mailing-list/page.tsx
@@ -3,7 +3,7 @@ export default function MailingListPage() {
     <div className="flex h-screen w-full flex-col items-center justify-start pt-5 text-center">
       <iframe
         className="h-[600px] w-96 overflow-visible text-center"
-        src="https://cdn.forms-content-1.sg-form.com/367df1a6-5df1-11ee-a7ab-42ca0d4ff327"
+        src="https://cdn.forms-content-1.sg-form.com/9dd24eec-6653-11ee-8d29-5a03e5bb0af3"
       />
     </div>
   );
