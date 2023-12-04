4d53ffe5f6bc00fdf020af44f6de5cea49e9cb6d
SashaBayan <sashabayan@gmail.com>
2023-12-03 20:03:45 -0800

    fix mailing list iframe width

--- app/mailing-list/page.tsx
+++ app/mailing-list/page.tsx
@@ -2,7 +2,7 @@ export default function MailingListPage() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-start pt-5 text-center">
       <iframe
-        className="h-[600px] w-96 overflow-visible text-center"
+        className="h-[600px] w-full overflow-visible text-center"
         src="https://cdn.forms-content-1.sg-form.com/9dd24eec-6653-11ee-8d29-5a03e5bb0af3"
       />
     </div>
