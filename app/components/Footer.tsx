c4a65e412eca95bdc7a2a2d8a13a1cab3ea167a3
SashaBayan <sashabayan@gmail.com>
2023-12-15 09:37:38 -0800

    update footer year

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="flex h-80 w-full items-center justify-center bg-[#332A2A] font-light text-white">
-      © Sasha Bayan 2023
+      © Sasha Bayan 2024
     </footer>
   );
 }
