08b4f842e8c270bcc01e031835ff1cd7b87e6c3c
SashaBayan <sashabayan@gmail.com>
2023-12-21 19:30:28 -0800

    add Press section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { IntroSection } from "./components/IntroSection";
 import { MusicSection } from "./components/MusicSection";
 import { NavBar } from "./components/NavBar";
 import Head from "next/head";
+import { PressSection } from "./epk/sections/PressSection";
 
 export default function Home() {
   return (
@@ -35,6 +36,7 @@ export default function Home() {
       <main className="scroll-smooth">
         <NavBar />
         <IntroSection />
+        <PressSection pressQuotes={pressQuotes()} />
         <MusicSection />
         <BioSection />
         <ContactSection />
@@ -42,4 +44,28 @@ export default function Home() {
       </main>
     </>
   );
+}
+
+function pressQuotes() {
+  return [
+    {
+      author: "Yellow & Black",
+      quote:
+        "Sasha Bayan...has recently landed on our radar with his single, “montreux,” and it seems we can’t get enough of it. The song is a great experience from start to finish and will leave stuck on constant repeat for days.",
+      href: "https://yellowblackmusic.com/2023/11/27/sasha-bayan-montreux/",
+    },
+    {
+      author: "Rock The Pigeon",
+      quote:
+        "Pianos, strings, and light percussion fill additional space in [montreux], creating a dreamy environment where listeners can fall in love with the sounds of Sasha Bayan",
+      href: "https://rockthepigeon.com/2023/12/18/exploring-love-and-loss-sasha-bayans-montreux-takes-listeners-on-a-dreamy-musical-journey/",
+    },
+    {
+      author: "Para Pop",
+      quote:
+        "“montreux” takes listeners into a world brimming with emotions, where every word and note carries weight and meaning...Sasha Bayan effortlessly leaves a profound and lasting impression on the audience with this song.",
+      href: "https://parapop.net/sasha-bayan-montreux/",
+      note: "(translated from Indonesian)",
+    },
+  ];
 }
