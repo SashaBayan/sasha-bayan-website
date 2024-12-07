a0830e8712966335948a85ff84b57fea39a0acdd
Sasha Khadivian <sashabayan@gmail.com>
2024-12-06 16:54:04 -0800

    remove fragment

--- app/epk/sitar/page.tsx
+++ app/epk/sitar/page.tsx
@@ -10,32 +10,30 @@ import { Footer } from "@/app/components/Footer";
 
 export default function SitarEPKPage() {
   return (
-    <>
-      <main className="flex h-screen w-screen flex-col items-center justify-start scroll-smooth">
-        <Header
-          header={"Sasha Bayan"}
-          subheader={
-            "Classically trained sitarist blending tradition with contemporary artistry"
-          }
-        />
-        <Section
-          leftContent={
-            <div className="flex flex-col items-center justify-center gap-5 px-10">
-              <h2 className="text-2xl font-semibold">About</h2>
-              <CopyableSection title="Tagline" content={TAGLINE} />
-              <CopyableSection title="Short Bio" content={SHORT_BIO} />
-              <CopyableSection title="Long Bio" content={LONG_BIO} />
-            </div>
-          }
-          rightContent={<GallerySection />}
-        />
-        <Section
-          reverse
-          leftContent={<FollowSection />}
-          rightContent={<BookSection />}
-        />
-        <Footer />
-      </main>
-    </>
+    <main className="flex h-screen w-screen flex-col items-center justify-start scroll-smooth">
+      <Header
+        header={"Sasha Bayan"}
+        subheader={
+          "Classically trained sitarist blending tradition with contemporary artistry"
+        }
+      />
+      <Section
+        leftContent={
+          <div className="flex flex-col items-center justify-center gap-5 px-10">
+            <h2 className="text-2xl font-semibold">About</h2>
+            <CopyableSection title="Tagline" content={TAGLINE} />
+            <CopyableSection title="Short Bio" content={SHORT_BIO} />
+            <CopyableSection title="Long Bio" content={LONG_BIO} />
+          </div>
+        }
+        rightContent={<GallerySection />}
+      />
+      <Section
+        reverse
+        leftContent={<FollowSection />}
+        rightContent={<BookSection />}
+      />
+      <Footer />
+    </main>
   );
 }
